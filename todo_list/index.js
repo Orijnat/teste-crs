29e8eeca68362d12be9dc8a2be5fbefe50b5be68
Cledivan <cledivanleao@gmail.com>
2026-03-26 18:21:45 -0300

    identando o odigo novamente

--- todo_list/index.js
+++ todo_list/index.js
@@ -6,53 +6,50 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 
     function addTarefa(taskName, taskDesc, prioridade, data, i){
-
-    const tr= document.createElement('tr')
-    tr.innerHTML= `
-    <td>${taskName}</td>
-    <td>${taskDesc} </td>
-    <td>${prioridade}</td>
-    <td>${data}</td>
-    <td>
-        <button type="button" class="btn btn-success btn-concluido" data-index="${i}">Marcar como concluido </button>
-        <button type="button" class="btn btn-danger btn-excluir" data-index="${i}"> Excluir </button>
-    </td>
-    `;
-    tabela.appendChild(tr);
-    
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+            <td>${taskName}</td>
+            <td>${taskDesc}</td>
+            <td>${prioridade}</td>
+            <td>${data}</td>
+            <td>
+                <button type="button" class="btn btn-success btn-concluido" data-index="${i}">Marcar como concluido</button>
+                <button type="button" class="btn btn-danger btn-excluir" data-index="${i}">Excluir</button>
+            </td>
+        `;
+        tabela.appendChild(tr);
     }
 
     teste.addEventListener('click', () => {
         dados.forEach((a) => {
-        console.log(`Tarefa: ${a.taskName}, Descrição: ${a.taskDesc}, Prioridade: ${a.prioridade} data:${a.data}`);
+            console.log(`Tarefa: ${a.taskName}, Descrição: ${a.taskDesc}, Prioridade: ${a.prioridade} data:${a.data}`);
         });
     })
 
 
 
-        function criarRegistro(taskName, taskDesc,prioridade, data) {
-            const novoRegistro = { taskName: taskName, taskDesc: taskDesc, prioridade: prioridade, data: data};
-                dados.push(novoRegistro)
-            atualizar()
-        }
+    function criarRegistro(taskName, taskDesc, prioridade, data) {
+        const novoRegistro = { taskName: taskName, taskDesc: taskDesc, prioridade: prioridade, data: data};
+        dados.push(novoRegistro);
+        atualizar();
+    }
 
     function deletar(i) {
         dados.splice(i, 1);
         atualizar();
     }
 
-    form.addEventListener("submit", (evento) =>{
+    form.addEventListener("submit", (evento) => {
         evento.preventDefault();
 
-        const taskName= document.getElementById('taskName').value.trim();
-        const taskDesc= document.getElementById('taskDesc').value.trim();
-        const prioridade= document.getElementById('Prioridade').value.trim();
-        const data= document.getElementById('data').value.trim();
+        const taskName = document.getElementById('taskName').value.trim();
+        const taskDesc = document.getElementById('taskDesc').value.trim();
+        const prioridade = document.getElementById('Prioridade').value.trim();
+        const data = document.getElementById('data').value.trim();
         alert("dados enviados com sucesso");
 
-
         criarRegistro(taskName, taskDesc, prioridade, data);
-})
+    })
 
 
     tabela.addEventListener('click', (evento) => {
@@ -64,31 +61,17 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 
     function atualizar() {
-        form.reset()
-        tabela.innerHTML= `
-            <th>
-                Nome da tarefa
-            </th>
-
-            <th>
-                Descricao da Tarefa
-            </th>
-            
-            <th>
-                Prioridade
-            </th>
-            
-            <th>
-                Data de Criaçao
-            </th>
-
-            <th>
-                Ações
-            </th>      
-        `
+        form.reset();
+        tabela.innerHTML = `
+            <th>Nome da tarefa</th>
+            <th>Descricao da Tarefa</th>
+            <th>Prioridade</th>
+            <th>Data de Criaçao</th>
+            <th>Ações</th>
+        `;
         dados.forEach((a, i) => {
-            addTarefa(a.taskName, a.taskDesc, a.prioridade, a.data, i)
-        })
+            addTarefa(a.taskName, a.taskDesc, a.prioridade, a.data, i);
+        });
     }
 })
 
